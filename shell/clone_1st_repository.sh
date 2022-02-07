f855a02f972b243bfd5da870ba470d970f1f7ca5
capoony <capoony@gmail.com>
2022-02-07 13:12:06 +0100

    initial commit

---
+++ shell/clone_1st_repository.sh
@@ -0,0 +1,17 @@
+## create github directory in home folder
+
+mkdir ~/github
+
+cd ~/github
+
+git config --global --list
+
+# clone My1stRepo repository
+
+## note that you need your access tocken for that
+
+firefox https://github.atom.io/login
+
+# then enter the tocken when prompted
+
+git clone https://capoony@github.com/capoony/My1stRep
